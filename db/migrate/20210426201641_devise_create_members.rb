bb185e4a95e39eb47cba45448131334b97c0995d
Guillermo Martinez <dumblemoe@gmail.com>
2021-04-28 08:19:24 -0500

    Deleted name attribute from members table in db.

--- db/migrate/20210426201641_devise_create_members.rb
+++ db/migrate/20210426201641_devise_create_members.rb
@@ -6,7 +6,6 @@ class DeviseCreateMembers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :first_name,               null: false, default: ""
       t.string :last_name,               null: false, default: ""       
-      t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
